9b0ec42bd5df1962c0e60e5f9127ad79f8375ec7
oldplum <oldplum@outlook.com>
2026-03-16 11:45:58 +0800

    chore: stop tracking build artifacts

--- build/CMakeFiles/breakout_week2.dir/Ball.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/breakout_week2.dir/Ball.cpp.o: /home/oldplum/3D_Game/Ball.cpp \
- /usr/include/stdc-predef.h /home/oldplum/3D_Game/Ball.h \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h
--- build/CMakeFiles/breakout_week2.dir/Brick.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/breakout_week2.dir/Brick.cpp.o: \
- /home/oldplum/3D_Game/Brick.cpp /usr/include/stdc-predef.h \
- /home/oldplum/3D_Game/Brick.h /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h
